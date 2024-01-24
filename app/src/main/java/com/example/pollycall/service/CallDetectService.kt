c98d6e1d69b188bd40da79e9a30156b34265c8c3
Pollyanna Wu <pollyanna.wu@gogolook.com>
2024-01-24 18:52:26 +0800

    Feat: Create a service to detect call

---
+++ app/src/main/java/com/example/pollycall/service/CallDetectService.kt
@@ -0,0 +1,24 @@
+package com.example.pollycall.service
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class CallDetectService: Service() {
+
+    private fun startDetectCall(){
+        // start the service when app create
+    }
+
+    private fun getCallData(){
+        // get the call data from repository
+    }
+
+    private fun blockScamCall(){
+
+    }
+    override fun onBind(p0: Intent?): IBinder? {
+       // TODO: Return the block message to activity
+        return null
+    }
+}
